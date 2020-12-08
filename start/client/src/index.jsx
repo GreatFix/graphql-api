0c29557302712bb8e49c499f5b1cbe8aeb781b94
У М <umalat.magma@gmail.com>
2020-12-08 14:28:28 +0300

    step 07

--- start/client/src/index.jsx
+++ start/client/src/index.jsx
@@ -1,8 +1,22 @@
-import { ApolloClient } from "@apollo/client";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
 import { cache } from "./cache";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import injectStyles from "./styles";
 
+// Initialize ApolloClient
 const client = new ApolloClient({
   cache,
   uri: "http://localhost:5000/graphql"
 });
 
+injectStyles();
+
+// Pass the ApolloClient instance to the ApolloProvider component
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
